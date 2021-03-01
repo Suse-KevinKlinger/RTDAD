970314bd7dd47b99f3c42abf65c65cb5721b7dbe
Kevin Klinger <kevin.klinger@suse.com>
2021-03-01 10:25:25 +0100

    Ceph secrets are variables now

--- variables.tf
+++ variables.tf
@@ -113,3 +113,14 @@ variable "k8s_namespace" {
   type        = string
   default     = "di"
 }
+
+variable "ceph_admin_secret" {
+  description = "The key that can be used to access the SES storage as admin user"
+  type        = string
+  default     = ""
+}
+
+variable "ceph_user_secret" {
+  description = "The key that can be used to access the SES storage as desired user"
+  type        = string
+  default     = ""
